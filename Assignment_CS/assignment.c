17ea0d8fb46bfbe1514013a7b6a0949c7cbc9421
Ayush Gaur <154548799+Ayu440@users.noreply.github.com>
2025-04-07 20:35:44 +0530

    Update assignment.c

--- Assignment_CS/assignment.c
+++ Assignment_CS/assignment.c
@@ -1,8 +1,33 @@
-#include<stdio.h>
-#include"ayush.h"
-int main(){
-    pyramid(5);
-    //upsidedown_pyramid(5);
-    //even_or_odd(3);
-      
+#include <stdio.h>
+#include "ayush.h"
+
+int main() {
+    
+    printf("Sum: %d\n", sum(3, 4));
+    printf("Difference: %d\n", diff(7, 2));
+    printf("Product: %d\n", product(3, 5));
+    printf("Division: %d\n", divide(10, 2));
+
+    char buf1[1000], buf2[1000];
+    pyramid(4, buf1);
+    upsidedown_pyramid(4, buf2);
+    printf("\nPyramid:\n%s", buf1);
+    printf("\nUpside Down Pyramid:\n%s", buf2);
+
+    int n = 121;
+    if (palindrome(n))
+        printf("\n%d is a palindrome\n", n);
+    else
+        printf("\n%d is not a palindrome\n", n);
+
+    int arr[] = {5, 3, 8, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bubble_sort(arr, size);
+    printf("\nSorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
